de4c460b1c9cd41521703fed4953e5e6ec3bb565
LouisRoyLangevin <150217238+LouisRoyLangevin@users.noreply.github.com>
2024-02-05 13:05:38 -0500

    first big update

---
+++ script.js
@@ -0,0 +1,8 @@
+function gobackhover(){
+    document.getElementById("go-back").style.color = "rgb(170,170,170)"
+    document.getElementById("go-back-icon").src = "images/go-back-icon-hover.png"
+}
+function gobacknothover(){
+    document.getElementById("go-back").style.color = "rgb(70,70,70)"
+    document.getElementById("go-back-icon").src = "images/go-back-icon.png"
+}

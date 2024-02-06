0a4fb89316346f05962c3b7e2cc6df0881bcfdcc
LouisRoyLangevin <150217238+LouisRoyLangevin@users.noreply.github.com>
2024-02-05 22:20:29 -0500

    made the home page better

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ function gobacknothover(){
     document.getElementById("go-back").style.color = "rgb(70,70,70)"
     document.getElementById("go-back-icon").src = "images/go-back-icon.png"
 }
+
+function linkedinhover(){
+    document.getElementById("linked-in-text").style.color = "grey"
+}
+function linkedinnothover(){
+    document.getElementById("linked-in-text").style.color = "black"
+}
+
+function githubhover(){
+    document.getElementById("github-text").style.color = "grey"
+}
+function githubnothover(){
+    document.getElementById("github-text").style.color = "black"
+}
+
+window.addEventListener('pageshow', githubnothover);
+window.addEventListener('pageshow', linkedinnothover);
